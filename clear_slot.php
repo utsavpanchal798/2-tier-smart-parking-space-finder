40cf28bf53cb2d67038fbc578d637d74c30e1587
Utsav Panchal <utsavpanchal798@gmail.com>
2026-05-24 11:40:54 +0530

    Update clear_slot.php

--- clear_slot.php
+++ clear_slot.php
@@ -5,11 +5,9 @@ if (isset($_POST['clear_now'])) {
     $slot_id = intval($_POST['slot_id']);
     $area_id = intval($_POST['area_id']);
 
-    // ૧. પાર્કિંગ સ્લોટનું સ્ટેટસ પાછું 'Available' કરવું
     $update_query = "UPDATE parking_slots SET status = 'Available' WHERE id = $slot_id";
     
     if ($conn->query($update_query) === TRUE) {
-        // ૨. બુકિંગ ટેબલમાંથી એન્ટ્રી ડીલીટ કરવી (જેથી સ્લોટ ફ્રી થઈ જાય)
         $delete_query = "DELETE FROM bookings WHERE slot_id = $slot_id";
         $conn->query($delete_query);
         
